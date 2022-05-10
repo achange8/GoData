6267fdab79c43182f33f6c37aaad200285baaa7f
achange8 <95342381+achange8@users.noreply.github.com>
2022-05-10 19:13:52 +0900

    update

--- DataStruck.go
+++
@@ -1,5 +0,0 @@
-package DataStruck
-
-func DataStruck() {
-
-}
--- main.go
+++ main.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import "fmt"
 
+type Node struct {
+	next *Node
+	val  int
+}
+
+func main() {
+	var root *Node
+
+	root = &Node{val: 0}
+
+	for i := 1; i < 10; i++ {
+		AddNode(root, i)
+	}
+	node := root
+
+	for node.next != nil {
+		fmt.Printf("%d -> ", node.val)
+		node = node.next
+	}
+	fmt.Printf("%d\n", node.val)
+}
+
+func AddNode(root *Node, val int) {
+	var endP *Node
+	endP = root
+	for endP.next != nil {
+		endP = endP.next
+	}
+
+	node := &Node{val: val}
+	endP.next = node
 }
