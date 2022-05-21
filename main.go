5326e13b43ec22cc020bc77a59936ab1c6dd73ae
achange8 <95342381+achange8@users.noreply.github.com>
2022-05-21 18:26:20 +0900

    Update main.go
    check code

--- main.go
+++ main.go
@@ -2,18 +2,23 @@ package main
 
 import (
 	"fmt"
-	"main/dataStrucks"
+	"regexp"
 )
 
 func main() {
 
-	stack2 := dataStrucks.NewStack()
-	for i := 1; i <= 5; i++ {
-		stack2.Push(i)
-	}
-	fmt.Println("New Stack")
-	for !stack2.Empty() {
-		val := stack2.Pop()
-		fmt.Printf("%d ->", val)
-	}
+	validEmail, _ := regexp.Compile(
+		"^[_a-z0-9+-.]+@[a-z0-9-]+(\\.[a-z0-9-]+)*(\\.[a-z]{2,4})$",
+	)
+	fmt.Println(validEmail.MatchString("gildong@example.com"))
+
+	// stack2 := dataStrucks.NewStack()
+	// for i := 1; i <= 5; i++ {
+	// 	stack2.Push(i)
+	// }
+	// fmt.Println("New Stack")
+	// for !stack2.Empty() {
+	// 	val := stack2.Pop()
+	// 	fmt.Printf("%d ->", val)
+	// }
 }
