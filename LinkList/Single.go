48998a04077523578be79d5ac00a054e54266d99
unknown <achange888@gmail.com>
2023-02-22 17:39:11 +0900

    learn single linked list

--- LinkList/Single.go
+++ LinkList/Single.go
@@ -7,6 +7,8 @@ type SNode[T any] struct {
 type LinkedList[T any] struct {
 	root *SNode[T]
 	tail *SNode[T]
+
+	count int
 }
 
 // add node back
@@ -14,6 +16,7 @@ func (l *LinkedList[T]) PushBack(value T) {
 	node := &SNode[T]{
 		value: value,
 	}
+	l.count++
 	if l.root == nil {
 		l.root = node
 		l.tail = node
@@ -28,6 +31,8 @@ func (l *LinkedList[T]) PushFront(value T) {
 	node := &SNode[T]{
 		value: value,
 	}
+	l.count++
+
 	if l.root == nil {
 		l.root = node
 		l.tail = node
@@ -41,6 +46,91 @@ func (l LinkedList[T]) Front() *SNode[T] {
 	return l.root
 }
 
-func (l LinkedList[T]) back() *SNode[T] {
+func (l LinkedList[T]) Back() *SNode[T] {
 	return l.tail
+}
+
+// O(N)
+func (l LinkedList[T]) CountNode() int {
+	node := l.root
+	cnt := 0
+
+	for ; node != nil; node = node.next {
+		cnt++
+	}
+	return cnt
+}
+
+// O(1)
+func (l LinkedList[T]) CountNode2() int {
+	return l.count
+}
+
+func (l LinkedList[T]) GetAt(idx int) *SNode[T] {
+	if idx >= l.CountNode2() {
+		return nil
+	}
+
+	i := 0
+
+	for node := l.root; node != nil; node = node.next {
+		if i == idx {
+			return node
+		}
+		i++
+	}
+	return nil
+}
+
+func (l *LinkedList[T]) InsertAfter(node *SNode[T], value T) {
+	if !l.isinclude(node) {
+		return
+	}
+
+	newNode := &SNode[T]{
+		value: value,
+	}
+
+	orgiNext := node.next
+	node.next = newNode
+	newNode.next = orgiNext
+
+	l.count++
+}
+
+func (l LinkedList[T]) InsertBefore(node *SNode[T], value T) {
+	if node == l.root {
+		l.PushFront(value)
+		return
+	}
+	PrevNode := l.findPrevNode(node)
+	if PrevNode == nil {
+		return
+	}
+
+	newNode := &SNode[T]{
+		value: value,
+	}
+
+}
+
+func (l LinkedList[T]) findPrevNode(node *SNode[T]) *SNode[T] {
+	inner := l.root
+	for ; inner != nil; inner = inner.next {
+		if inner.next == node {
+			return inner
+		}
+	}
+	return nil
+}
+
+func (l LinkedList[T]) isinclude(node *SNode[T]) bool {
+	inner := l.root
+
+	for ; inner != nil; inner = inner.next {
+		if inner == node {
+			return true
+		}
+	}
+	return false
 }
--- LinkList/Single_test.go
+++ LinkList/Single_test.go
@@ -15,10 +15,63 @@ func TestPushBack(t *testing.T) {
 
 	assert.NotNil(t, l.root)
 	assert.Equal(t, 1, l.Front().value)
-	assert.Equal(t, 1, l.back().value)
+	assert.Equal(t, 1, l.Back().value)
 
 	l.PushBack(2)
 	assert.NotNil(t, l.root)
 	assert.Equal(t, 1, l.Front().value)
-	assert.Equal(t, 2, l.back().value)
+	assert.Equal(t, 2, l.Back().value)
+}
+
+func TestPushFront(t *testing.T) {
+	var l LinkedList[int]
+
+	assert.Nil(t, l.root)
+	assert.Nil(t, l.tail)
+
+	l.PushFront(1)
+	assert.NotNil(t, l.root)
+	assert.Equal(t, 1, l.Front().value)
+	assert.Equal(t, 1, l.Back().value)
+
+	l.PushFront(2)
+	assert.NotNil(t, l.root)
+	assert.Equal(t, 2, l.Front().value)
+	assert.Equal(t, 1, l.Back().value)
+
+	l.PushFront(4) // node) 4->2->1
+	assert.NotNil(t, l.root)
+	assert.Equal(t, 4, l.Front().value)
+	assert.Equal(t, 1, l.Back().value)
+
+	assert.Equal(t, 3, l.CountNode())
+	assert.Equal(t, 3, l.CountNode2())
+
+	assert.Equal(t, 4, l.GetAt(0).value)
+	assert.Equal(t, 2, l.GetAt(1).value)
+	assert.Equal(t, 1, l.GetAt(2).value)
+
+}
+
+func TestInsertAfter(t *testing.T) {
+	var l LinkedList[int]
+
+	l.PushBack(1)
+	l.PushBack(2)
+	l.PushBack(3)
+
+	node := l.GetAt(1)
+	l.InsertAfter(node, 4)
+
+	assert.Equal(t, 4, l.CountNode2())
+	assert.Equal(t, 4, l.GetAt(2).value)
+	assert.Equal(t, 3, l.Back().value)
+
+	tempNode := &SNode[int]{
+		value: 100,
+	}
+	l.InsertAfter(tempNode, 200)
+
+	assert.Equal(t, 4, l.CountNode())
+	assert.Equal(t, 4, l.CountNode2())
 }
